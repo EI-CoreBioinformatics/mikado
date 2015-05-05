ff29be846388ded9707293682e2d74cadd4f9e2e
Luca Venturini <Luca.Venturini@tgac.ac.uk>
2015-05-05 10:27:09 +0100

    BF on add_transcript_to_gtf.py

--- util/add_transcript_feature_to_gtf.py
+++ util/add_transcript_feature_to_gtf.py
@@ -11,43 +11,47 @@ import argparse
 
 class obj(object): pass
 
-current=obj()
-current.transcript=None
+def main():
 
-rows=[]
+    current=obj()
+    current.transcript=None
+    
+    rows=[]
+    
+    parser=argparse.ArgumentParser("Script to add a transcript feature to e.g. Cufflinks GTFs")
+    parser.add_argument("gtf", type=argparse.FileType("r"),
+                        help="Input GTF")
+    parser.add_argument("out", default=sys.stdout, nargs="?",
+                        type=argparse.FileType("w"),
+                        help="Output file. Default: stdout.")
+    args=parser.parse_args()
+    
+    args.gtf.close()
+    
+    for record in GTF(args.gtf.name):
+            if current.transcript!=record.transcript:
+                    if current.transcript!=None:
+                            print(current, file=args.out)
+                            exon_no=0
+                            for row in filter(lambda x: x.feature=="exon", sorted(rows, key=operator.attrgetter("start"))):
+                                    exon_no+=1
+                                    row.info["exon_number"]=exon_no
+                                    print(row, file=args.out)
+                            exon_no=0
+                            for row in filter(lambda x: x.feature=="CDS", sorted(rows, key=operator.attrgetter("start"))):
+                                    exon_no+=1
+                                    row.info["exon_number"]=exon_no
+                                    print(row, file=args.out)
+                    rows=[record]
+                    current=deepcopy(record)
+                    current.feature="transcript"
+                    
+            else:
+                    current.end=max(current.end,record.end)
+                    current.start=min(current.start, record.start)
+                    rows.append(record)
+    
+    print(current, file=args.out)
+    for row in rows: print(row, file=args.out)
 
-parser=argparse.ArgumentParser("Script to add a transcript feature to e.g. Cufflinks GTFs")
-parser.add_argument("gtf", type=argparse.FileType("r"),
-                    help="Input GTF")
-parser.add_argument("out", default=sys.stdout, nargs="?",
-                    type=argparse.FileType("w"),
-                    help="Output file. Default: stdout.")
-args=parser.parse_args()
-
-args.gtf.close()
-
-for record in GTF(args.gtf.name):
-        if current.transcript!=record.transcript:
-                if current.transcript!=None:
-                        print(current, file=args.out)
-                        exon_no=0
-                        for row in filter(lambda x: x.feature=="exon", sorted(rows, key=operator.attrgetter("start"))):
-                                exon_no+=1
-                                row.info["exon_number"]=exon_no
-                                print(row, file=args.out)
-                        exon_no=0
-                        for row in filter(lambda x: x.feature=="CDS", sorted(rows, key=operator.attrgetter("start"))):
-                                exon_no+=1
-                                row.info["exon_number"]=exon_no
-                                print(row, file=args.out)
-                rows=[record]
-                current=deepcopy(record)
-                current.feature="transcript"
-                
-        else:
-                current.stop=max(current.stop,record.stop)
-                current.start=min(current.start, record.start)
-                rows.append(record)
-
-print(current, file=args.out)
-for row in rows: print(row, file=args.out)
+if __name__=='__main__': main()

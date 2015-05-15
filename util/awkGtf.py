a4c097e20e558d039589a7f07047836c642a0edd
Luca Venturini (TGAC) <venturil@v0267.tgaccluster>
2015-05-15 11:33:32 +0100

    Added some utilities

---
+++ util/awkGtf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys,os
+sys.path.append(
+    os.path.dirname(
+        os.path.dirname(__file__)
+        )
+    )
+import argparse
+from loci_objects.GTF import GTF
+from loci_objects.transcript import transcript
+
+def main():
+
+    parser=argparse.ArgumentParser("Quick utility to extract features from a GTF with certain coordinates.")
+    parser.add_argument("--chrom", required=True)
+    parser.add_argument("--start", type=int, default=float("-inf"))
+    parser.add_argument("--end", type=int, default=float("inf"))
+    parser.add_argument("gtf", type=argparse.FileType("r"))
+    parser.add_argument("out", type=argparse.FileType("w"), nargs="?", default=sys.stdout)
+    args=parser.parse_args()
+
+    currentTranscript=None
+    with GTF(args.gtf) as gtf:
+        for row in gtf:
+            if row.chrom!=args.chrom:
+                continue
+            else:
+                if row.is_transcript is True:
+                    if currentTranscript is not None and currentTranscript.start>=args.start and currentTranscript.end<=args.end:
+                        print(currentTranscript.__str__(to_gtf=True), file=args.out)
+                    currentTranscript=transcript(row)
+                else:
+                    currentTranscript.addExon(row)
+
+    if currentTranscript is not None and currentTranscript.start>=args.start and currentTranscript.end<=args.end:
+        print(currentTranscript.__str__(to_gtf=True), file=args.out)
+
+if __name__=="__main__":
+    main()
+            
+        
+                        
+                        
+                
+                
+                    
+                    
+    
---
+++ util/getFastaFromIds.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys, argparse, os
+from Bio import SeqIO
+from io import IOBase
+
+def main():
+
+	def check_type(string):
+		if os.path.exists(string) and not os.path.isdir(string): return open(string)
+		else: return set(string.split(','))
+
+	parser=argparse.ArgumentParser(description='A simple script that retrieves the FASTA sequences from a file given a list of ids.')
+	parser.add_argument("-v", "--reverse", action="store_true", default=False, help="Retrieve entries which are not in the list, as in grep -v (a homage).")
+	parser.add_argument('list', type=check_type, help='File with the list of the ids to recover, one by line. Alternatively, names separated by commas.')
+	parser.add_argument('fasta', type=argparse.FileType('r'), help='FASTA file.')
+	parser.add_argument('out', type=argparse.FileType('w'), help='Optional output file.', nargs='?', default=sys.stdout)
+	args=parser.parse_args()
+
+	if isinstance(args.list, IOBase):
+		ids = set([line.rstrip() for line in args.list.readlines()])
+	else: ids=args.list
+	for record in SeqIO.parse(args.fasta, 'fasta'):
+		if record.id in ids:
+			if args.reverse: continue
+			print(record.format('fasta'), file=args.out, end='')
+			ids.remove(record.id)
+			if len(ids)==0 and not args.reverse: break
+		else:
+			if args.reverse:
+				print(record.format('fasta'), file=args.out, end='')
+
+if __name__=='__main__': main()

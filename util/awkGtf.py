86d00c595005a3a694d019868c284e6cb91f8767
Luca Venturini <Luca.Venturini@tgac.ac.uk>
2015-05-15 14:42:39 +0100

    Improvement for awkGTF

--- util/awkGtf.py
+++ util/awkGtf.py
@@ -14,6 +14,8 @@ def main():
 
     parser=argparse.ArgumentParser("Quick utility to extract features from a GTF with certain coordinates.")
     parser.add_argument("--chrom", required=True)
+    parser.add_argument("-as", "--assume-sorted", dest="assume_sorted",
+                        default=False, action="store_true")
     parser.add_argument("--start", type=int, default=float("-inf"))
     parser.add_argument("--end", type=int, default=float("inf"))
     parser.add_argument("gtf", type=argparse.FileType("r"))
@@ -21,11 +23,14 @@ def main():
     args=parser.parse_args()
 
     currentTranscript=None
+    chromFound=False
     with GTF(args.gtf) as gtf:
         for row in gtf:
             if row.chrom!=args.chrom:
                 continue
             else:
+                if chromFound is False: chromFound=True
+                if args.assume_sorted is True and row.end>args.end: break
                 if row.is_transcript is True:
                     if currentTranscript is not None and currentTranscript.start>=args.start and currentTranscript.end<=args.end:
                         print(currentTranscript.__str__(to_gtf=True), file=args.out)

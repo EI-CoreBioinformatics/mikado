be57c0904c70d4c1c64709707e6c0cb646a9e37d
Luca Venturini <Luca.Venturini@tgac.ac.uk>
2015-06-05 20:00:36 +0100

    Added a utility to convert GFF=> BED12

---
+++ util/gffjunc_to_bed12.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import sys,os,argparse
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import loci_objects
+
+def main():
+    
+    parser=argparse.ArgumentParser("GFF=>BED12 converter")
+    parser.add_argument("gff", type=argparse.FileType("r"))
+    parser.add_argument("out", nargs="?", type=argparse.FileType("w"), default=sys.stdout)
+    args=parser.parse_args()
+    
+    currentTranscript=None
+    
+    for row in loci_objects.GFF.GFF3(args.gff):
+        if row.is_parent and not row.is_transcript:
+            continue
+        if row.is_exon:
+            currentTranscript.addExon(row)
+        elif row.is_transcript:
+            if currentTranscript is not None:
+                currentTranscript.finalize()
+                assert len(currentTranscript.introns)==1
+                bed12=loci_objects.bed12.BED12()
+                bed12.chrom=currentTranscript.chrom
+                bed12.strand=currentTranscript.strand
+                bed12.start=currentTranscript.start
+                bed12.end=currentTranscript.end
+                bed12.thickStart=currentTranscript.introns[0][0]
+                bed12.thickEnd=currentTranscript.introns[0][1]
+                bed12.name=currentTranscript.id
+                bed12.rgb="255,0,0"
+                bed12.blockCount=2
+                bed12.blockSizes=[(e[1]-e[0]) for e in currentTranscript.exons  ]
+                bed12.blockStarts=[]
+                bed12.blockStarts=[0, bed12.blockSizes[0]+currentTranscript.introns[0][1]-currentTranscript.introns[0][0] ] 
+                print(bed12, file=args.out)
+                                           
+            currentTranscript=loci_objects.transcript.transcript(row)
+            
+    if currentTranscript is not None:
+        currentTranscript.finalize()
+        assert len(currentTranscript.introns)==1
+        bed12=loci_objects.bed12.BED12()
+        bed12.chrom=currentTranscript.chrom
+        bed12.strand=currentTranscript.strand
+        bed12.start=currentTranscript.start
+        bed12.end=currentTranscript.end
+        bed12.thickStart=currentTranscript.introns[0][0]
+        bed12.thickEnd=currentTranscript.introns[0][1]
+        bed12.name=currentTranscript.id
+        bed12.rgb="255,0,0"
+        bed12.blockCount=2
+        bed12.blockSizes=[(e[1]-e[0]) for e in currentTranscript.exons  ]
+        bed12.blockStarts=[]
+        bed12.blockStarts=[0, bed12.blockSizes[0]+currentTranscript.introns[0][1]-currentTranscript.introns[0][0] ] 
+        print(bed12, file=args.out)
+
+if __name__=="__main__": main()
+  
+            

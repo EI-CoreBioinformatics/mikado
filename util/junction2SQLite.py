c9aa75249d020c116141b5a11c4184f394f1b582
Luca Venturini <Luca.Venturini@tgac.ac.uk>
2015-06-09 14:35:02 +0100

    Minor modifications to the SQLite utils

--- util/junction2SQLite.py
+++ util/junction2SQLite.py
@@ -5,11 +5,12 @@ from shanghai_lib.serializers import junction
 def main():
     parser=argparse.ArgumentParser()
     parser.add_argument("--fai", default=None)
+    parser.add_argument("--max-objects", dest="max_objects", default=10**5, type=int)
     parser.add_argument("bed12")
     parser.add_argument("db")
     args=parser.parse_args()
     
-    serializer=junction.junctionSerializer(args.bed12, args.db, fai=args.fai)
+    serializer=junction.junctionSerializer(args.bed12, args.db, fai=args.fai, maxobjects=args.max_objects)
     serializer()
     
 if __name__=="__main__": main()
--- util/orf2SQLite.py
+++ util/orf2SQLite.py
@@ -6,13 +6,14 @@ from Bio import SeqIO
 def main():
     parser=argparse.ArgumentParser("Simple script to serialise ORF BED files into the SQLite DB.")
     parser.add_argument("--fasta", default=None)
+    parser.add_argument("-mo", "--max-objects", dest="max_objects", type=int, default=10**5)
     parser.add_argument("bed12")
     parser.add_argument("db")
     args=parser.parse_args()
     if args.fasta is not None:
         args.fasta=SeqIO.index(args.fasta, "fasta")
     
-    serializer=orf.orfSerializer(args.bed12, args.db, fasta_index=args.fasta)
+    serializer=orf.orfSerializer(args.bed12, args.db, fasta_index=args.fasta, maxobjects=args.max_objects)
     serializer.serialize()
     
 if __name__=="__main__": main()

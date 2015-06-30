00721800c787d261569d1683666f3211dd9d5453
Luca Venturini <Luca.Venturini@tgac.ac.uk>
2015-06-30 11:07:17 +0100

    Changed version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, "DESCRIPTION.md"), encoding="utf-8") as description:
 setup(
 
     name = "shanghai",
-    version = "0.3",
+    version = "0.4",
     
     description="A Python3 annotation program to select the best gene model in each locus",
     long_description=long_description,

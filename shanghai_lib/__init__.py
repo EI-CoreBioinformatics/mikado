774acfef10e5d5e66db889ec561e9b54cc150bb8
Luca Venturini <Luca.Venturini@tgac.ac.uk>
2015-06-30 10:52:29 +0100

    BF for __init__.py

--- shanghai_lib/__init__.py
+++ shanghai_lib/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 #coding: utf_8
 
+__all__ = ["parsers", "loci_objects", "serializers", "exceptions", "json_utils", "scales"]
+
 import shanghai_lib.parsers
 import shanghai_lib.loci_objects
 import shanghai_lib.serializers

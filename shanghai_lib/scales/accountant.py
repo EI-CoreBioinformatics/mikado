eb6229b31940ece6f5bf33a3a3af0cb1e18cb2f0
Luca Venturini <Luca.Venturini@tgac.ac.uk>
2015-07-01 21:50:56 +0100

    BF for accountant

--- shanghai_lib/scales/accountant.py
+++ shanghai_lib/scales/accountant.py
@@ -174,7 +174,8 @@ class accountant:
             if other_exon is not None:
                 assert type(other_exon) is tuple
                 assert other_exon in self.exons[tr.chrom][s], (tr.id, tr.exons, other_exon)
-                assert 0b100 & self.exons[tr.chrom][s][other_exon], (tr.id, tr.exons, other_exon) #Check the other exon is marked as single 
+                if not 0b100 & self.exons[tr.chrom][s][other_exon]:
+                    self.exons[tr.chrom][s][other_exon] |= 0b100 #Check the other exon is marked as single 
 #                 self.exons[tr.chrom][s][exon] |= 0b100000
                 self.exons[tr.chrom][s][other_exon] |= 0b100000
 

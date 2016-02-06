12849e9acabcdc3719374573c5a76f4141fb14a9
Luca Venturini <luca.venturini@tgac.ac.uk>
2016-02-06 15:53:19 +0000

    Reinstated the check for metric calculation, which should lead to a huge speed-up

--- mikado_lib/loci_objects/abstractlocus.py
+++ mikado_lib/loci_objects/abstractlocus.py
@@ -9,6 +9,7 @@ import abc
 import random
 import logging
 from sys import maxsize
+from copy import copy, deepcopy
 import intervaltree
 import networkx
 from ..exceptions import NotInLocusError
@@ -406,7 +407,12 @@ class Abstractlocus(metaclass=abc.ABCMeta):
 
         self.start = min(self.start, transcript.start)
         self.end = max(self.end, transcript.end)
-        self.transcripts[transcript.id] = transcript
+
+        # Copying the transcript ensures that the modifications we make DOWNSTREAM
+        # will not be passed back upstream; e.g., eventual metrics which are set from outside multiple times
+        # (intron/exon fraction, etc) will stay stable at each point.
+        # This increases slighlty memory usage but is worth it for my sanity when analysing the tables.
+        self.transcripts[transcript.id] = copy(transcript)
         self.splices.update(transcript.splices)
         self.introns.update(transcript.introns)
         if transcript.monoexonic is False:

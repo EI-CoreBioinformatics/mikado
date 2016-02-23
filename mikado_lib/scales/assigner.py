4a697cbc57ae36ebae1a58629fae3c3b470dc6fc
Luca Venturini <luca.venturini@tgac.ac.uk>
2016-02-23 11:03:45 +0000

    BF for Assigner.compare

--- mikado_lib/scales/assigner.py
+++ mikado_lib/scales/assigner.py
@@ -710,7 +710,7 @@ class Assigner:
 
         if (prediction.strand != reference.strand and
                 all([x is not None for x in (prediction.strand, reference.strand)])):
-            if ccode in ("e", "o", "c", "m", "_"):
+            if ccode in ("e", "o", "c", "m", "_", "C"):
                 ccode = "x"
             elif ccode not in ("u", "i", "I", "p", "P", "x"):
                 # print(ccode)

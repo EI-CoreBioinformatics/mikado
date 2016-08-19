39ebed03309d346a1c45aee26e3aa7adeed8ea6f
Luca Venturini <Luca.Venturini@tgac.ac.uk>
2016-08-19 20:21:45 +0100

    BF for readTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.

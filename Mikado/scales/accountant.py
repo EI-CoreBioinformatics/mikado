3c3ba8d29a56145cafa90a0f6667b23417199030
Luca Venturini <lucventurini@gmail.com>
2019-04-04 15:56:57 +0100

    Redundant-intron calculation is now correct again. Now trying to fix the redundant one.

--- Mikado/scales/accountant.py
+++ Mikado/scales/accountant.py
@@ -268,35 +268,44 @@ class Accountant:
 
         intron_stats = [0, 0, 0, 0]  # Common - pred side, common - ref side, prediction, reference
         intron_nr_stats = [0, 0, 0]  # Common, prediction, reference
+
         splice_stats = [0, 0, 0]  # Common, prediction, reference
 
         for chrom in self.introns:
             for strand in self.introns[chrom]:
-                splice_starts = [set(), set()]  # prediction, reference
+                intron_nrs = [collections.Counter(), collections.Counter()]
+                splice_starts = [set(), set()]  # reference, prediction
                 splice_ends = [set(), set()]
                 for intron in self.introns[chrom][strand]:
-                    if self.introns[chrom][strand][intron][0]:
+                    if self.introns[chrom][strand][intron][0]:  # reference
                         splice_starts[0].add(intron[0])
                         splice_ends[0].add(intron[1])
-                    if self.introns[chrom][strand][intron][1]:
+                    if self.introns[chrom][strand][intron][1]:  # prediction
                         splice_starts[1].add(intron[0])
                         splice_ends[1].add(intron[1])
 
-                    if ((self.introns[chrom][strand][intron][0]) &
-                                           (self.introns[chrom][strand][intron][1])):
-                        intron_stats[0] += self.introns[chrom][strand][intron][1]
-                        intron_stats[1] += self.introns[chrom][strand][intron][0]
-                        intron_nr_stats[0] += 1
-
-                    intron_stats[2] += self.introns[chrom][strand][intron][1]
-                    intron_stats[3] += self.introns[chrom][strand][intron][0]
-
-                    intron_nr_stats[1] += (self.introns[chrom][strand][intron][1] > 0)
-                    intron_nr_stats[2] += (self.introns[chrom][strand][intron][0] > 0)
+                    intron_nrs[0][intron] += self.introns[chrom][strand][intron][0]
+                    intron_nrs[1][intron] += self.introns[chrom][strand][intron][1]
 
                 splice_stats[0] += len(set.intersection(*splice_starts)) + len(set.intersection(*splice_ends))
-                splice_stats[1] += len(splice_starts[0]) + len(splice_ends[0])
-                splice_stats[2] += len(splice_starts[1]) + len(splice_ends[1])
+                splice_stats[2] += len(splice_starts[1]) + len(splice_ends[1])  # prediction
+                splice_stats[1] += len(splice_starts[0]) + len(splice_ends[0])  # reference
+
+                for key in set.union(set(intron_nrs[0].keys()), set(intron_nrs[1].keys())):
+                    __ref_val, __pred_val = intron_nrs[0][key], intron_nrs[1][key]
+                    if min(__ref_val, __pred_val) > 0:
+                        intron_stats[1] += __ref_val
+                        intron_stats[0] += __pred_val
+                        intron_nr_stats[0] += 1
+
+                    intron_stats[2] += __pred_val
+                    intron_stats[3] += __ref_val
+                    intron_nr_stats[1] += (__pred_val > 0)
+                    intron_nr_stats[2] += (__ref_val > 0)
+
+        self.logger.info("Intron stats: {}".format(", ".join([str(_) for _ in intron_stats])))
+        # intron_nr_stats = [len(set.intersection(*intron_nrs)), len(intron_nrs[1]), len(intron_nrs[0])]
+        self.logger.info("Intron NR stats: {}".format(", ".join([str(_) for _ in intron_nr_stats])))
 
         # Common, prediction, reference
         intron_chains_nonred = [0, 0, 0]
@@ -545,7 +554,7 @@ class Accountant:
                 for intron in accountant.introns[chrom][strand]:
                     if intron not in self.introns[chrom][strand]:
                         self.introns[chrom][strand][intron] = [0, 0]
-                    self.introns[chrom][strand][intron][0] += accountant.introns[chrom][strand][intron][0]
+                    # self.introns[chrom][strand][intron][0] += accountant.introns[chrom][strand][intron][0]
                     self.introns[chrom][strand][intron][1] += accountant.introns[chrom][strand][intron][1]
 
         self.monoexonic_matches[0].update(accountant.monoexonic_matches[0])
@@ -871,7 +880,7 @@ class Accountant:
         )
 
         intron_precision, intron_recall, intron_f1 = self.__calculate_redundant_statistics(
-            *intron_results["intron_chains"]["redundant"]
+            *intron_results["introns"]["redundant"]
         )
 
         intron_nr_precision, intron_nr_recall, intron_nr_f1 = self.__calculate_statistics(

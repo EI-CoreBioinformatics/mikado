EI-CoreBioinformatics/mikado

commit 83ae1fd40661f856742952e2fee0ab2f596f6fca

This commit introduces the following: - Transcript and AbstractLocus-derived classes will not be hashable any longer. This to conform to the Python model for which a hash should be stable across the lifetime of an object; something that cannot be guaranteed with either class. - Reorganised the code, especially for Mikado.scales. This should lead to much faster start-up times for the CLI invokation. - Now the parsing classes will use the default Python iterator to raise StopIteration (not the line == '' hack present before).

Mikado/daijin/mikado.smk
@@ -2,7 +2,7 @@ import os
 from shutil import which
 import pkg_resources
 from Bio.Data import CodonTable
-from Mikado.serializers.blast_serializer.tabular_utils import blast_keys
+from ..serializers.blast_serializer.tabular_utils import blast_keys
 import functools
 import subprocess
 import re

Mikado/loci/abstractlocus.py
@@ -75,7 +75,7 @@ class Abstractlocus(metaclass=abc.ABCMeta):
         self.logger = logger
         self.__stranded = False
         self._not_passing = set()
-        self._excluded_transcripts = set()
+        self._excluded_transcripts = dict()
         self.transcripts = dict()
         self.start, self.end, self.strand = maxsize, -maxsize, None
         # Consider only the CDS part
@@ -136,11 +136,6 @@ class Abstractlocus(metaclass=abc.ABCMeta):
             if getattr(self, feature) != getattr(other, feature):
                 return False
         return True
-
-    def __hash__(self):
-        """This has to be defined, otherwise abstractloci objects won't be hashable
-        (and therefore operations like adding to sets will be forbidden)"""
-        return super().__hash__()
 
     def __len__(self):
         return self.end - self.start + 1
@@ -528,7 +523,9 @@ class Abstractlocus(metaclass=abc.ABCMeta):
         """This method is needed to exchange transcripts that might have been modified by padding."""
         if original_transcript.tid != transcript.tid:
             raise KeyError("I cannot hot swap two transcripts with two different IDs!")
-        if hash(original_transcript) == hash(transcript):  # Expensive operation, let us try to avoid it if possible
+        # if hash(original_transcript) == hash(transcript):  # Expensive operation, let us try to avoid it if possible
+        #     return
+        if original_transcript == transcript:
             return
 
         self.logger.debug("Swapping %s with a new transcript", original_transcript.id)
@@ -1256,7 +1253,8 @@ class Abstractlocus(metaclass=abc.ABCMeta):
 
         if self.scores_calculated is True:
             # assert self.transcripts[list(self.transcript.keys())[0]].score is not None
-            test = set.difference(set(self.transcripts.keys()), self._excluded_transcripts)
+            test = set.difference(set(self.transcripts.keys()),
+                                  set(self._excluded_transcripts.keys()))
             if test and self.transcripts[test.pop()].score is None:
                 for tid in self.transcripts:
                     if tid in self._excluded_transcripts:
@@ -1375,7 +1373,7 @@ class Abstractlocus(metaclass=abc.ABCMeta):
                     self.transcripts[tid].score = 0
                 else:
                     self.logger.debug("Excluding %s from %s because of failed requirements", tid, self.id)
-                    self._excluded_transcripts.add(self.transcripts[tid])
+                    self._excluded_transcripts[tid] = self.transcripts[tid]
                     self.remove_transcript_from_locus(tid)
 
             if not self.purge:

Mikado/subprograms/prepare.py
@@ -12,8 +12,7 @@ import logging
 import logging.handlers
 from ..utilities import path_join, parse_list_file
 from ..utilities.log_utils import formatter
-from ..configuration.configurator import to_json, check_json
-from Mikado.exceptions import InvalidJson
+from ..exceptions import InvalidJson
 import random
 from collections import Counter
 
@@ -204,8 +203,9 @@ def setup(args):
     if isinstance(args.json_conf["reference"]["genome"], bytes):
         args.json_conf["reference"]["genome"] = args.json_conf["reference"]["genome"].decode()
 
+    from ..configuration.configurator import to_json, check_json
     try:
-        args.json_conf = check_json(args.json_conf)
+        args.json_conf = check_json(to_json(args.json_conf))
     except InvalidJson as exc:
         logger.exception(exc)
         raise exc
@@ -309,7 +309,7 @@ score must be a valid floating number.
     parser.add_argument("-of", "--out_fasta", default=None,
                         help="Output file. Default: mikado_prepared.fasta.")
     parser.add_argument("--json-conf", dest="json_conf",
-                        type=to_json, default="",
+                        type=str, default="",
                         help="Configuration file.")
     parser.add_argument("-k", "--keep-redundant", default=None,
                         dest="keep_redundant", action="store_true",

.gitignore
@@ -63,9 +63,9 @@ Mikado/utilities/overlap.cpython-3*.so
 Mikado/scales/contrast.c
 Mikado/scales/contrast.cpython-3*.so
 Mikado/scales/contrast.html
-Mikado/scales/f1.html
-Mikado/scales/f1.c
-Mikado/scales/f1.cpython-3*.so
+Mikado/utilities/f1.html
+Mikado/utilities/f1.c
+Mikado/utilities/f1.cpython-3*.so
 Mikado/utilities/overlap.html
 Mikado/scales/intervaltree.cpython-3*.so
 doc/build/sample_data/configuration.yaml

Mikado/configuration/configurator.py
@@ -682,7 +682,9 @@ def to_json(string, simple=False, logger=None):
         logger = create_default_logger("to_json")
 
     try:
-        if string is None or string == '' or string == dict():
+        if isinstance(string, dict):
+            json_dict = string
+        elif string is None or string == '' or string == dict():
             json_dict = dict()
             string = os.path.join(os.path.abspath(os.getcwd()), "mikado.json")
         else:
@@ -699,6 +701,7 @@ def to_json(string, simple=False, logger=None):
                     json_dict = json.loads(json_file.read())
         json_dict["filename"] = string
         json_dict = check_json(json_dict, simple=simple, logger=logger)
+
     except Exception as exc:
         raise OSError((exc, string))
 

Mikado/loci/locus.py
@@ -8,16 +8,15 @@ i.e. the locus.
 import collections
 import itertools
 import operator
-from collections import deque, defaultdict
+from collections import defaultdict
 import pysam
 from ..transcripts.transcript import Transcript
 from ..transcripts.transcriptchecker import TranscriptChecker
 from .abstractlocus import Abstractlocus, rgetattr
 from ..parsers.GFF import GffLine
-from ..scales.assigner import Assigner
+from ..scales.assignment.assigner import Assigner
 from ..exceptions import InvalidTranscript
 import networkx as nx
-import numpy as np
 import io
 from pkg_resources import resource_stream
 import rapidjson as json

Mikado/loci/sublocus.py
@@ -218,12 +218,13 @@ class Sublocus(Abstractlocus):
         self.logger.debug("Launching calculate scores for {0}".format(self.id))
         self.calculate_scores()
 
-        if self._excluded_transcripts and self.purge:
-            while self._excluded_transcripts:
-                excluded = self._excluded_transcripts.pop()
-                self.excluded.add_transcript_to_locus(excluded,
+        if len(self._excluded_transcripts) > 0 and self.purge:
+            excluded_tids = list(self._excluded_transcripts.keys())
+            for excluded_tid in excluded_tids:
+                self.excluded.add_transcript_to_locus(self._excluded_transcripts[excluded_tid],
                                                       check_in_locus=False)
-                self.remove_transcript_from_locus(excluded.id)
+                self.remove_transcript_from_locus(excluded_tid)
+                del self._excluded_transcripts[excluded_tid]
 
         self.logger.debug("Defining monosubloci for {0}".format(self.id))
 

Mikado/loci/superlocus.py
@@ -26,11 +26,11 @@ from ..serializers.external import External
 from ..serializers.junction import Junction, Chrom
 from ..serializers.orf import Orf
 from ..utilities import dbutils, grouper
-from ..scales.assigner import Assigner
+from ..scales.assignment.assigner import Assigner
 import bisect
 from sys import maxsize
 import functools
-import numpy as np
+
 if version_info.minor < 5:
     from sortedcontainers import SortedDict
 else:
@@ -507,13 +507,15 @@ class Superlocus(Abstractlocus):
                                                        introns=self.locus_verified_introns,
                                                        session=self.session,
                                                        data_dict=data_dict)
-        to_remove, to_add = False, set()
+        to_remove, to_add = False, dict()
 
         if self.json_conf["pick"]["chimera_split"]["execute"] is True:
             if self.transcripts[tid].number_internal_orfs > 1:
                 new_tr = list(self.transcripts[tid].split_by_cds())
                 if len(new_tr) > 1:
-                    to_add.update(new_tr)
+                    for new in new_tr:
+                        assert new.id not in to_add
+                        to_add[new.id] = new
                     to_remove = True
                     self.logger.info("%s has been split into %d different transcripts.",
                                      tid, len(new_tr))
@@ -715,7 +717,7 @@ class Superlocus(Abstractlocus):
             self.logger.debug("Length of data dict: %s", len(data_dict))
 
         tid_keys = list(self.transcripts.keys())
-        to_remove, to_add = set(), set()
+        to_remove, to_add = set(), dict()
         # This will function even if data_dict is None
         self._load_introns(data_dict)
 
@@ -730,7 +732,6 @@ class Superlocus(Abstractlocus):
                               len(tid_keys))
             self.session.close()
             sasession.close_all_sessions()
-            # self.sessionmaker.close_all_sessions()
 
         for tid in tid_keys:
             remove_flag, new_transcripts = self.load_transcript_data(tid, data_dict)
@@ -745,8 +746,8 @@ class Superlocus(Abstractlocus):
                               len(to_remove))
             for tid in to_remove:
                 self.remove_transcript_from_locus(tid)
-            for transcript in to_add:
-                self.logger.debug("Adding %s to %s", transcript.id, self.id)
+            for tid, transcript in to_add.items():
+                self.logger.debug("Adding %s to %s", tid, self.id)
                 self.add_transcript_to_locus(transcript, check_in_locus=False)
 
         elif len(to_remove) == len(self.transcripts):
@@ -945,10 +946,13 @@ class Superlocus(Abstractlocus):
 
         # Check whether there is something to remove
         self._check_requirements()
-        while self._excluded_transcripts:
-            to_remove = self._excluded_transcripts.pop()
+        excluded_tids = list(self._excluded_transcripts.keys())
+        for excluded_tid in excluded_tids:
+            to_remove = self._excluded_transcripts[excluded_tid]
             self.excluded.add_transcript_to_locus(to_remove, check_in_locus=False)
             self.remove_transcript_from_locus(to_remove.id)
+            if excluded_tid in self._excluded_transcripts:
+                del self._excluded_transcripts[excluded_tid]
 
         if len(self.transcripts) == 0:
             # we have removed all transcripts from the Locus. Set the flag to True and exit.
@@ -1243,7 +1247,8 @@ class Superlocus(Abstractlocus):
             loci_transcripts.update(set([_ for _ in locus.transcripts.keys()]))
 
         not_loci_transcripts = set.difference({_ for _ in self.transcripts.keys()
-                                               if _ not in self._excluded_transcripts}, loci_transcripts)
+                                               if _ not in self._excluded_transcripts},
+                                              loci_transcripts)
 
         for locus in self.loci.values():
             to_remove = set()
@@ -1427,7 +1432,8 @@ class Superlocus(Abstractlocus):
 
         # As we are using intern for transcripts, this should prevent
         # memory usage to increase too much
-        objects = dict((tid, item) for tid, item in self.transcripts.items() if tid not in self._excluded_transcripts)
+        objects = dict((tid, item) for tid, item in self.transcripts.items() if tid not in
+                       self._excluded_transcripts)
         graph.add_nodes_from(objects.keys())
 
         monos = []

commit c6dfc476ced47bdb854902c98d7db6401acf0d6a

Solving a couple of bugs introduced by 83ae1fd40661f856742952e2fee0ab2f596f6fca. Following fe2db80767ed87dd8e517436580b19bfe5b33b11, modified the __eq__ comparator for loci as well.

Mikado/daijin/mikado.smk
@@ -2,7 +2,7 @@ import os
 from shutil import which
 import pkg_resources
 from Bio.Data import CodonTable
-from ..serializers.blast_serializer.tabular_utils import blast_keys
+from Mikado.serializers.blast_serializer.tabular_utils import blast_keys
 import functools
 import subprocess
 import re

Mikado/loci/abstractlocus.py
@@ -130,12 +130,12 @@ class Abstractlocus(metaclass=abc.ABCMeta):
     def __eq__(self, other):
         if not isinstance(self, type(other)):
             return False
-        for feature in ["chrom", "strand", "start",
-                        "end", "exons", "introns",
-                        "splices", "stranded"]:
-            if getattr(self, feature) != getattr(other, feature):
-                return False
-        return True
+
+        return (
+            self.chrom, self.strand, self.start, self.end, self.stranded,
+            self.introns, self.splices, self.exons
+        ) == (other.chrom, other.strand, other.start, other.end, other.stranded,
+              other.introns, other.splices, other.exons)
 
     def __len__(self):
         return self.end - self.start + 1

Mikado/preparation/prepare.py
@@ -331,7 +331,8 @@ def perform_check(keys, shelve_stacks, args, logger):
         }
 
         working_processes = []
-        for idx, batch in enumerate(np.array_split(batches, args.json_conf["threads"]), 1):
+        for idx, batch in enumerate(np.array_split(np.array(batches,
+                                                            dtype=object), args.json_conf["threads"]), 1):
             batch_file = tempfile.NamedTemporaryFile(delete=False, mode="wb")
             msgpack.dump(batch.tolist(), batch_file)
             batch_file.flush()

Mikado/serializers/blast_serializer/tabular_utils.py
@@ -390,7 +390,8 @@ def parse_tab_blast(self,
             pfile.write(msgpack.dumps(params))
         assert os.path.exists(params_file)
         # Split the indices
-        for idx, split in enumerate(np.array_split(np.array(list(groups.items())), procs)):
+        for idx, split in enumerate(np.array_split(np.array(list(groups.items()),
+                                                            dtype=object), procs)):
             with open(index_files[idx], "wb") as index:
                 for item in split:
                     vals = (tuple(item[0]), values[item[1], :].tolist())

Mikado/subprograms/prepare.py
@@ -108,6 +108,9 @@ def setup(args):
     logger = logging.getLogger("prepare")
     logger.setLevel(logging.INFO)
 
+    from ..configuration.configurator import to_json
+    args.json_conf = to_json(args.json_conf)
+
     if args.start_method:
         args.json_conf["multiprocessing_method"] = args.start_method
 
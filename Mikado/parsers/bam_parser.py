87a37a1c7c3db7cfc72c4c936f35a0bf3c3d99ba
Luca Venturini <luca.venturini@earlham.ac.uk>
2021-01-13 20:25:03 +0000

    Load from bam (#359)
    * Initial bam parser work + test
    
    * Fixed BAM parsing
    
    * Added the pacbio.bam file for the sample data.
    
    * Now Mikado should be able to handle BAM files without the @SQ headers
    
    * Closes #355. Fixed the bug introduced in the previous commit.
    
    Co-authored-by: ljyanesm <yanes.luis@gmail.com>

--- Mikado/parsers/bam_parser.py
+++ Mikado/parsers/bam_parser.py
@@ -8,10 +8,14 @@ class BamParser(Parser):
 
     def __init__(self, handle):
         self.__closed = False
-        self._handle = pysam.AlignmentFile(handle, "rb")
+        self._handle = pysam.AlignmentFile(handle, "rb", check_sq=False)
 
     def __iter__(self):
         return self
 
     def __next__(self):
         return next(self._handle)
+
+    @property
+    def name(self):
+        return self._handle.filename.decode()

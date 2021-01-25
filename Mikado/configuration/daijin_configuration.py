f29f570f590fda45c105c9d7bc3cb03770425bdf
Luca Venturini <luca.venturini@earlham.ac.uk>
2021-01-25 16:54:33 +0000

    Finished the Daijin configuration

--- Mikado/configuration/daijin_configuration.py
+++ Mikado/configuration/daijin_configuration.py
@@ -1,15 +1,12 @@
 from dataclasses import field
-from marshmallow_dataclass import dataclass
+from marshmallow_dataclass import dataclass, Optional, List
 from marshmallow import validate
-from typing import List
 from .picking_config import PickConfiguration
 from .prepare_config import PrepareConfiguration
 from .serialise_config import SerialiseConfiguration
 from ..utilities.dbutils import DBConfiguration
 from ..utilities.log_utils import LoggingConfiguration
 from .configuration import ReferenceConfiguration
-from typing import List, Optional
-from marshmallow import Schema, fields
 import copy
 
 
@@ -21,113 +18,127 @@ class ShortReads:
     strandedness: List[str] = field(default_factory=lambda: [])
 
 
-class LongReads(Schema):
-    files: list = fields.List(fields.Str(missing=""), missing=lambda: [""])
-    samples: list = fields.List(fields.Str(missing=""), missing=lambda: [""])
-    strandedness: list = fields.List(fields.Str(missing=""), missing=lambda: [""])
-    skip_split: bool = fields.Bool(missing=True)
+@dataclass
+class LongReads:
+    files: List[str] = field(default_factory=lambda: [])
+    samples: List[str] = field(default_factory=lambda: [])
+    strandedness: List[str] = field(default_factory=lambda: [])
+    skip_split: bool = field(default=True)
 
 
-class OrfCalling(Schema):
-    min_protein_len: int = fields.Int(missing=30)
-    execute: bool = fields.Bool(missing=True)
+@dataclass
+class OrfCalling:
+    min_protein_len: int = field(default=30, metadata={"validate": validate.Range(min=0)})
+    execute: bool = field(default=True)
 
 
-class BlastX(Schema):
-    prot_db: list = fields.List(fields.Str(missing=""), missing=lambda: [""])
-    evalue: float = fields.Float(missing=1e-7)
-    max_target_seqs: int = fields.Int(missing=10)
-    chunks: int = fields.Int(missing=10)
+@dataclass
+class BlastX:
+    prot_db: Optional[List[str]] = field(default_factory=lambda: [""])
+    evalue: float = field(default=1e-7, metadata={"validate": validate})
+    max_target_seqs: int = field(default=10, metadata={"validate": validate.Range(min=1)})
+    chunks: int = field(default=10, metadata={"validate": validate.Range(min=1)})
 
 
-class AlignMethods(Schema):
-    tophat: list = fields.List(fields.Str(missing=''), missing=lambda: [""])
-    hisat: list = fields.List(fields.Str(missing=''), missing=lambda: [""])
-    star: list = fields.List(fields.Str(missing=''), missing=lambda: [""])
-    gsnap: list = fields.List(fields.Str(missing=''), missing=lambda: [""])
+@dataclass
+class AlignMethods:
+    tophat: Optional[List[str]] = field(default=None)
+    hisat: Optional[List[str]] = field(default=None)
+    star: Optional[List[str]] = field(default=None)
+    gsnap: Optional[List[str]] = field(default=None)
 
 
-class LongReadAlign(Schema):
-    star: list = fields.List(fields.Str(missing=''), missing=lambda: [""])
-    gmap: list = fields.List(fields.Str(missing=''), missing=lambda: [""])
+@dataclass
+class LongReadAlign:
+    star: Optional[List[str]] = field(default=None)
+    gmap: Optional[List[str]] = field(default=None)
 
 
-class AsmMethods(Schema):
-    stringtie: list = fields.List(fields.Str(missing=''), missing=lambda: [""])
-    cufflinks: list = fields.List(fields.Str(missing=''), missing=lambda: [""])
-    class2: list = fields.List(fields.Str(missing=''), missing=lambda: [""])
-    scallop: list = fields.List(fields.Str(missing=''), missing=lambda: [""])
-    trinity: list = fields.List(fields.Str(missing=''), missing=lambda: [""])
+@dataclass
+class AsmMethods:
+    stringtie: Optional[List[str]] = field(default=None)
+    cufflinks: Optional[List[str]] = field(default=None)
+    class2: Optional[List[str]] = field(default=None)
+    scallop: Optional[List[str]] = field(default=None)
+    trinity: Optional[List[str]] = field(default=None)
 
 
-class ProgramLoader(Schema):
-    tophat: str = fields.Str(missing='')
-    gmap: str = fields.Str(missing='')
-    star: str = fields.Str(missing='')
-    hisat: str = fields.Str(missing='')
-    samtools: str = fields.Str(missing='')
-    cufflinks: str = fields.Str(missing='')
-    scallop: str = fields.Str(missing='')
-    trinity: str = fields.Str(missing='')
-    stringtie: str = fields.Str(missing='')
-    class2: str = fields.Str(missing='')
-    transdecoder: str = fields.Str(missing='')
-    prodigal: str = fields.Str(missing='')
-    portcullis: str = fields.Str(missing='')
-    mikado: str = fields.Str(missing='')
-    diamond: str = fields.Str(missing='')
-    blast: str = fields.Str(missing='')
+@dataclass
+class ProgramLoader:
+    tophat: str = field(default='')
+    gmap: str = field(default='')
+    star: str = field(default='')
+    hisat: str = field(default='')
+    samtools: str = field(default='')
+    cufflinks: str = field(default='')
+    scallop: str = field(default='')
+    trinity: str = field(default='')
+    stringtie: str = field(default='')
+    class2: str = field(default='')
+    transdecoder: str = field(default='')
+    prodigal: str = field(default='')
+    portcullis: str = field(default='')
+    mikado: str = field(default='')
+    diamond: str = field(default='')
+    blast: str = field(default='')
 
 
-class Portcullis(Schema):
-    do: bool = fields.Bool(missing=True)
-    canonical_juncs: str = fields.Str(missing="C,S")
+@dataclass
+class Portcullis:
+    do: bool = field(default=True)
+    canonical_juncs: str = field(default="C,S")
 
 
-class AlnIndex(Schema):
-    star: str = fields.Str(missing="")
+@dataclass
+class AlnIndex:
+    star: str = field(default="")
 
 
-class DaijinMikadoConfiguration(Schema):
-    use_diamond: bool = fields.Bool(missing=True)
-    use_prodigal: bool = fields.Bool(missing=True)
-    modes: List[str] = fields.List(fields.Str(missing=''), missing=lambda: ["stringent"])
+@dataclass
+class DaijinMikadoConfiguration:
+    use_diamond: bool = field(default=True)
+    use_prodigal: bool = field(default=True)
+    modes: List[str] = field(default_factory=lambda: ["stringent"])
 
 
-class DaijinConfiguration(Schema):
+@dataclass
+class DaijinConfiguration:
     # Daijin specific sub-modules
-    short_reads: ShortReads = fields.Nested(ShortReads)
-    long_reads: LongReads = fields.Nested(LongReads)
-    orf_calling: OrfCalling = fields.Nested(OrfCalling)
-    blastx: BlastX = fields.Nested(BlastX)
-    aln_index: AlnIndex = fields.Nested(AlnIndex)
-    align_methods: AlignMethods = fields.Nested(AlignMethods)
-    long_read_align_methods: LongReadAlign = fields.Nested(LongReadAlign)
-    asm_methods: AsmMethods = fields.Nested(AsmMethods)
-    portcullis: Portcullis = fields.Nested(Portcullis)
-    mikado: DaijinMikadoConfiguration = fields.Nested(DaijinMikadoConfiguration)
+    short_reads: ShortReads = field(default_factory=ShortReads)
+    long_reads: LongReads = field(default_factory=LongReads)
+    orf_calling: OrfCalling = field(default_factory=OrfCalling)
+    blastx: BlastX = field(default_factory=BlastX)
+    aln_index: AlnIndex = field(default_factory=AlnIndex)
+    align_methods: AlignMethods = field(default_factory=AlignMethods)
+    long_read_align_methods: LongReadAlign = field(default_factory=LongReadAlign)
+    asm_methods: AsmMethods = field(default_factory=AsmMethods)
+    portcullis: Portcullis = field(default_factory=Portcullis)
+    mikado: DaijinMikadoConfiguration = field(default_factory=DaijinMikadoConfiguration)
     #
-    filename: Optional[str] = fields.Str(missing=None)
-    seed: int = fields.Int(missing=0)
-    multiprocessing_method: str = fields.Str(missing="spawn")
-    log_settings: LoggingConfiguration = fields.Nested(LoggingConfiguration)
-    db_settings: DBConfiguration = fields.Nested(DBConfiguration)
-    serialise: SerialiseConfiguration = fields.Nested(SerialiseConfiguration)
-    prepare: PrepareConfiguration = fields.Nested(PrepareConfiguration)
-    pick: PickConfiguration = fields.Nested(PickConfiguration)
-    reference: ReferenceConfiguration = fields.Nested(ReferenceConfiguration)
-    name: str = fields.Str(missing="Daijin")
-    out_dir: str = fields.Str(missing="daijin")
-    threads: int = fields.Int(missing=4)
-    scheduler: str = fields.Str(missing=None)
-    #
+    filename: Optional[str] = field(default=None)
+    seed: int = field(default=0, metadata={"validate": validate.Range(min=0, max=2**32 - 1)})
+    multiprocessing_method: str = field(default="spawn",
+                                        metadata={"validate": validate.OneOf(["spawn", "fork", "fork-server"])})
+    log_settings: LoggingConfiguration = field(default_factory=LoggingConfiguration)
+    db_settings: DBConfiguration = field(default_factory=DBConfiguration)
+    serialise: SerialiseConfiguration = field(default_factory=SerialiseConfiguration)
+    prepare: PrepareConfiguration = field(default_factory=PrepareConfiguration)
+    pick: PickConfiguration = field(default_factory=PickConfiguration)
+    reference: ReferenceConfiguration = field(default_factory=ReferenceConfiguration)
+    name: str = field(default="Daijin")
+    out_dir: str = field(default="Daijin")
+    threads: int = field(default=4, metadata={"validate": validate.Range(min=1)})
+    scheduler: Optional[str] = field(default=None,
+                                     metadata={"validate": validate.OneOf(["SLURM", "LSF", "local", "PBS", ""])})
+
     # # These fields are loaded *from the scoring configuration*
-    scoring: Optional[dict] = fields.Dict(missing=None)
-    cds_requirements: Optional[dict] = fields.Dict(missing=None)
-    as_requirements: Optional[dict] = fields.Dict(missing=None)
-    requirements: Optional[dict] = fields.Dict(missing=None)
-    not_fragmentary: Optional[dict] = fields.Dict(missing=None)
-    load: ProgramLoader = fields.Nested(ProgramLoader)
+    # TODO test this
+    scoring: Optional[dict] = field(default_factory=dict, default=None)
+    cds_requirements: Optional[dict] = field(default_factory=dict, default=None)
+    as_requirements: Optional[dict] = field(default_factory=dict, default=None)
+    requirements: Optional[dict] = field(default_factory=dict, default=None)
+    not_fragmentary: Optional[dict] = field(default_factory=dict, default=None)
+    load: ProgramLoader = field(default_factory=ProgramLoader)
 
     def copy(self):
         return copy.deepcopy(self)

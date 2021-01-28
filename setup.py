bdbfbd95ee87ade0e03be8c6dfd174ddb9024db0
Luca Venturini <luca.venturini@earlham.ac.uk>
2021-01-28 17:53:37 +0000

    Updated the requirements.txt and the setup.py

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
         "Framework :: Pytest",
         "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3.6",
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
         "Programming Language :: Python :: 3.8",
         'Programming Language :: Python :: 3.9'
     ],

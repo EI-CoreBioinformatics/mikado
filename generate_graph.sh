8913bc55c3a5037b327e39023f98ac27380f8840
Luca Venturini <luca.venturini@earlham.ac.uk>
2021-02-08 21:55:44 +0000

    Removed for now the graph generation script

--- generate_graph.sh
+++
@@ -1,8 +0,0 @@
-# Script to generate a call graph for Mikado using pyan3
-
-pip install pyan3
-
-cd Mikado/
-cat <(echo __init__.py) <(find _transcripts configuration daijin daijin loci parsers picking preparation scales serializers subprograms transcripts utilities  -iname "*py") | xargs pyan3 --dot --no-defines -G -a --colored -e -g > ../Mikado.dot
-cd ../
-cat Mikado.dot | dot -Tsvg -Granksep=1.5 > Mikado.svg

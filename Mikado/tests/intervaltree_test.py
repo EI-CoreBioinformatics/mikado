6e69f7d5c04b512842bd142d001c34d97198d08e
Luca Venturini <luca.venturini@earlham.ac.uk>
2021-02-18 10:44:46 +0000

    BROKEN. Starting to modify @brentp improvements to the old intervaltree to port to Mikado.

--- Mikado/tests/intervaltree_test.py
+++ Mikado/tests/intervaltree_test.py
@@ -5,38 +5,45 @@
 
 import numpy
 import unittest
-from ..utilities import Interval, IntervalNode, IntervalTree
+from ..utilities import Interval, IntervalTree
 
 
 class NeighborTestCase(unittest.TestCase):
 
     def setUp(self):
-        iv = IntervalNode(50, 59, Interval(50, 59))
-        for i in range(0, 110, 10):
+        iv = IntervalTree()
+        iv.insert(50, 59)
+        for i in range(10, 110, 10):
             if i == 50:
                 continue
-            f = Interval(i, i + 9, value=None)
-            iv = iv.insert(f.start, f.end, f)
+            # f = Interval(i, i + 9, value=None)
+            iv.insert(i, i+9)
+            self.assertEqual(iv.right(Interval(i, i+1), num_intervals=1)[0].start, i)
         self.intervals = iv
 
     def test_left(self):
         iv = self.intervals
-        self.assertEqual(str(iv.left(60, n=2)), str([Interval(50, 59), Interval(40, 49)]))
+        assert iv is not None
+        self.assertEqual(str(iv.left(Interval(60, 61),
+                                     num_intervals=2)), str([Interval(50, 59), Interval(40, 49)]))
 
         for i in range(10, 100, 10):
-            r = iv.left(i, max_dist=10, n=1)
+            r = iv.left(Interval(i, i+1), max_dist=10, num_intervals=1)
             self.assertEqual(r[0].end,  i - 1)
 
     def test_toomany(self):
         iv = self.intervals
-        self.assertEqual(len(iv.left(60, n=200)), 6)
+        self.assertEqual(len(iv.left(Interval(60, 61), num_intervals=200)), 6)
 
     def test_right(self):
         iv = self.intervals
-        self.assertEqual(str(iv.left(60, n=2)), str([Interval(50, 59), Interval(40, 49)]))
+        self.assertEqual(str(iv.left(Interval(60, 61),
+                                     num_intervals=2)), str([Interval(50, 59), Interval(40, 49)]))
+
+        self.assertEqual(iv.right(Interval(1, 2), max_dist=100, num_intervals=1)[0].start, 10)
 
         def get_right_start(b10):
-            r = iv.right(b10+1, n=1)
+            r = iv.right(Interval(b10, b10+1), num_intervals=1)
             assert len(r) == 1
             return r[0].start
 
@@ -44,7 +51,7 @@ class NeighborTestCase(unittest.TestCase):
             self.assertEqual(get_right_start(i), i + 10)
 
         for i in range(0, 100, 10):
-            r = iv.right(i-1, max_dist=10, n=1)
+            r = iv.right(Interval(i-1, i), max_dist=10, num_intervals=1)
             self.assertEqual(r[0].start, i)
 
 
@@ -106,39 +113,40 @@ class UpDownStreamTestCase(unittest.TestCase):
 class LotsaTestCase(unittest.TestCase):
     """ put lotsa data in the tree and make sure it works"""
     def setUp(self):
-        iv = IntervalNode(1, 2, Interval(1, 2))
+        iv = IntervalTree()
+        iv.insert(1, 2)
         self.max = 1000000
         for i in range(0, self.max, 10):
             f = Interval(i, i)
-            iv = iv.insert(f.start, f.end, f)
+            iv = iv.insert(f.start, f.end)
 
         for i in range(600):
-            iv = iv.insert( 0, 1, Interval(0, 1) )
+            iv = iv.insert_interval(Interval(0, 1))
         self.intervals = iv
 
     def test_count(self):
         iv = self.intervals
 
-        r = iv.right(1, n=33)
+        r = iv.right(1, num_intervals=33)
         self.assertEqual(len(r), 33)
 
-        l = iv.left(1, n=33)
+        l = iv.left(1, num_intervals=33)
         self.assertEqual(len(l), 1)
 
-        u = iv.right(1, n=9999)
+        u = iv.right(1, num_intervals=9999)
         self.assertEqual(len(u), 250)
 
         # now increase max_dist
-        u = iv.right(1, n=9999, max_dist=99999)
+        u = iv.right(1, num_intervals=9999, max_dist=99999)
         self.assertEqual(len(u), 9999)
 
     def test_max_dist(self):
         iv = self.intervals
-        r = iv.right(1, max_dist=0, n=10)
+        r = iv.right(1, max_dist=0, num_intervals=10)
         self.assertEqual(len(r), 0)
 
         for n, d in enumerate(range(10, 1000, 10)):
-            r = iv.right(1, max_dist=d, n=10000)
+            r = iv.right(1, max_dist=d, num_intervals=10000)
             self.assertEqual(len(r), n + 1)
 
     def test_find(self):
--- Mikado/utilities/intervaltree.pxd
+++ Mikado/utilities/intervaltree.pxd
@@ -1,35 +1,43 @@
 #distutils: language = c++
 
+cdef class Interval:
+    cdef public int start, end
+    cdef public object data
+    # cdef public int begin
+    cdef public object value, chrom, strand
+    cpdef tuple _as_tuple(Interval self)
+
+
 cdef class IntervalNode:
     cdef float priority
-    cdef public object interval
+    cdef public Interval interval
     cdef public int start, end
-    cdef int minend, maxend, minstart
+    cdef int minstop, maxstop, minstart
     cdef IntervalNode cleft, cright, croot
-
+    cpdef tuple _as_tuple(IntervalNode self)
     cdef IntervalNode rotate_right(IntervalNode self)
     cdef IntervalNode rotate_left(IntervalNode self)
-    cdef inline void set_ends(IntervalNode self)
+    cdef inline void set_stops(IntervalNode self)
     cdef void _intersect( IntervalNode self, int start, int end, list results)
     cdef void _seek_left(IntervalNode self, int position, list results, int n, int max_dist)
     cdef void _seek_right(IntervalNode self, int position, list results, int n, int max_dist)
     cdef void _traverse(IntervalNode self, object func)
-    cpdef right(self, position, int n=?, int max_dist=?)
-    cpdef left(self, position, int n=?, int max_dist=?)
-    cpdef IntervalNode insert(IntervalNode self, int start, int end, object value=*)
+    cdef IntervalNode _insert(IntervalNode self, Interval interval)
+    cpdef intersect(IntervalNode self, int start, int stop)
+    # cpdef right(self, Interval f, int n=1, int max_dist=25000)
+    cpdef left(IntervalNode self, Interval f, int num_intervals=?, int max_dist=?)
+    cpdef right(IntervalNode self, Interval f, int num_intervals=?, int max_dist=?)
+    # cpdef left(self, Interval f, int n=1, int max_dist=25000)
+    # cpdef IntervalNode insert(IntervalNode self, int start, int end, object value=*)
     cpdef bint fuzzy_equal(IntervalNode self, IntervalNode other, int fuzzy)
-
-
-cdef class Interval:
-    cdef public int start, end
-    cdef public int begin
-    cdef public object value, chrom, strand
-
-    cpdef tuple _as_tuple(self)
 
 
 cdef class IntervalTree:
     cdef IntervalNode root
     cdef int num_intervals
-    cpdef int size(self)
-    cdef inline void set_ends(IntervalTree self)
+    cpdef int size(IntervalTree self)
+    cpdef find(IntervalTree self, int start, int end, bint strict=?,
+               bint contained_check=?, int max_distance=?, int num_intervals=?, object value=?)
+    cpdef insert(IntervalTree self, int start, int end, object value=?)
+    cpdef insert_interval(IntervalTree self, Interval interval)
+    # cdef inline void set_stops(IntervalTree self)

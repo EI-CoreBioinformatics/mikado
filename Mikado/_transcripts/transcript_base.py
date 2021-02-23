EI-CoreBioinformatics/mikado

commit d10bd07d5eb55f79927265e75c035aea9f132c45

Solved the base issue causing the problems here: transcripts not dumping/reloading correctly

Mikado/_transcripts/transcript_base.py
@@ -198,7 +198,7 @@ class TranscriptBase:
 
         self.__combined_cds_length = 0
         self.__selected_cds = []
-        self.__cdna_length = None
+        self._cdna_length = None
         self._combined_cds_introns = set()
         self._selected_cds_introns = set()
         self.__selected_cds_locus_fraction = 0
@@ -240,9 +240,6 @@ class TranscriptBase:
         self.__cds_disrupted_by_ri = False
         self.exon_fraction = self.intron_fraction = 1
         self.cds_intron_fraction = self.selected_cds_intron_fraction = 1
-
-        # Json configuration
-        self.configuration = None
 
         # Things that will be populated by querying the database
         self.loaded_bed12 = []
@@ -446,8 +443,6 @@ class TranscriptBase:
             self.attributes[intern(key)] = val
 
         self.blast_hits = []
-        self.configuration = None
-
         if transcript_row.is_transcript is False:
             if transcript_row.is_exon is False and transcript_row.feature not in ("match", "tss", "tts"):
                 raise TypeError("Invalid GF line")
@@ -464,19 +459,8 @@ class TranscriptBase:
                 else:
                     self.id = transcript_row.transcript
                 self._possibly_without_exons = True
-                # if transcript_row.feature == "tts":  # Termination site, especially in Augustus
-                #     if self.strand == "-":
-                #         self.end = transcript_row.start
-                #     else:
-                #         self.end = transcript_row.end
-                # elif transcript_row.feature == "tss":  # Transcription start site, especially in Augustus
-                #     if self.strand == "-":
-                #         self.start = transcript_row.end
-                #     else:
-                #         self.start = transcript_row.start
-            elif transcript_row.is_exon is True and isinstance(transcript_row,
-                                                               GffLine) and transcript_row.feature not in (
-            "cDNA_match", "match"):
+            elif transcript_row.is_exon is True and isinstance(
+                    transcript_row, GffLine) and transcript_row.feature not in ("cDNA_match", "match"):
                 self.id = transcript_row.parent[0]
             else:
                 self.parent = transcript_row.gene
@@ -603,14 +587,10 @@ class TranscriptBase:
             del state["blast_baked"]
             del state["query_baked"]
 
-        state["__hit_num"] = len(self.blast_hits)
-
         return state
 
     def __setstate__(self, state):
-        hit_num = state.pop("__hit_num")
         self.__dict__.update(state)
-        assert len(self.blast_hits) == hit_num
         self._calculate_cds_tree()
         self._calculate_segment_tree()
         # Set the logger to NullHandler
@@ -1071,7 +1051,7 @@ exon data is on a different chromosome, {exon_data.chrom}. \
         self.internal_orfs, self.combined_utr = [], []
         self.segments = []
         # Need to recalculate it
-        self.__cdna_length = None
+        self._cdna_length = None
         self.finalize()
         if not (self.combined_utr == self.three_utr == self.five_utr == []):
             raise ValueError(
@@ -1146,7 +1126,7 @@ exon data is on a different chromosome, {exon_data.chrom}. \
         self.internal_orfs = []
         self.__internal_orf_transcripts = []
         self.combined_utr = []
-        self.__cdna_length = None
+        self._cdna_length = None
         self.finalized = False
 
     def reverse_strand(self):
@@ -1191,10 +1171,15 @@ exon data is on a different chromosome, {exon_data.chrom}. \
             state["exons"].append([exon[0], exon[1]])
         state["introns"] = list((intron[0], intron[1]) for intron in self.introns)
         state["verified_introns"] = list((intron[0], intron[1]) for intron in self.__verified_introns)
+        state["combined_cds_introns"] = list((intron[0], intron[1]) for intron in self.combined_cds_introns)
+        state["selected_cds_introns"] = list((intron[0], intron[1]) for intron in self.selected_cds_introns)
         state["original_source"] = self.__original_source
 
         state["orfs"] = dict()
         state["selected_orf"] = self.selected_internal_orf_index
+        state["selected_internal_orf_cds"] = []
+        for feature, (start, end), phase in self.__selected_internal_orf_cds:
+            state["selected_internal_orf_cds"].append([feature, [start, end], phase])
         for index, orf in enumerate(self.internal_orfs):
             state["orfs"][str(index)] = []
             for segment in orf:
@@ -1215,6 +1200,11 @@ exon data is on a different chromosome, {exon_data.chrom}. \
         state["combined_cds"] = list(self.combined_cds)
         # Now let'add the things that we calculate with the basic lengths
         state["calculated"] = self.__get_calculated_stats()
+        state["feature"] = self.feature
+        state["segments"] = []
+        for segment in self.segments:
+            segment = [segment[0], [segment[1][0], segment[1][1]]]
+            state["segments"].append(segment)
 
         for metric in mmetrics:
             state[metric] = getattr(self, metric)
@@ -1232,7 +1222,7 @@ exon data is on a different chromosome, {exon_data.chrom}. \
         :return:
         """
 
-        for key in ["chrom", "source",
+        for key in ["chrom", "source", "feature",
                     "start", "end", "strand", "score",
                     "parent", "id"]:
             if key not in state:
@@ -1247,59 +1237,44 @@ exon data is on a different chromosome, {exon_data.chrom}. \
 
         self.exons = []
         self.combined_cds = []
-        for exon in state["exons"]:
-            if len(exon) != 2:
-                raise CorruptIndex("Invalid exonic values for {}: {}".format(self.id, exon))
-            self.exons.append(tuple(exon))
-        for intron in state["introns"]:
-            if len(intron) != 2:
-                raise CorruptIndex("Invalid intronic values for {}: {}".format(self.id, intron))
-            self.introns.add(tuple(intron))
-        for intron in state["verified_introns"]:
-            if len(intron) != 2:
-                raise CorruptIndex("Invalid intronic values for {}: {}".format(self.id, intron))
-            self.__verified_introns.add(tuple(intron))
+        self._selected_cds_introns = set()
+        self._combined_cds_introns = set()
+        self.segments = []
+        self._cdna_length = None
+
+        for dump, store in zip(
+                ("exons", "introns", "verified_introns", "selected_cds_introns", "combined_cds_introns"),
+                (self.exons, self.introns, self.__verified_introns, self._selected_cds_introns,
+                 self._combined_cds_introns)):
+            for iv in state[dump]:
+                if len(iv) != 2:
+                    raise CorruptIndex("Invalid values for the {} for {}: {}".format(dump, self.id, iv))
+                if isinstance(store, set):
+                    store.add(tuple(iv))
+                else:
+                    store.append(tuple(iv))
+
+        for segment in state["segments"]:
+            if len(segment) != 2:
+                raise CorruptIndex("Invalid segment values for {}: {}".format(self.id, segment))
+            feature, (start, end) = segment
+            if not all([isinstance(feature, str), isinstance(start, int), isinstance(end, int)]):
+                raise CorruptIndex("Invalid segment values for {}: {}".format(self.id, segment))
+            self.segments.append((feature, (start, end)))
 
         self.splices = set(state["splices"])
-
         self._trust_orf = trust_orf
         self._accept_undefined_multi = accept_undefined_multi
         self.internal_orfs = []
-
         self.logger.debug("Starting to load the ORFs for %s", self.id)
-        try:
-            indices = dict((int(_), _) for _ in state["orfs"])
-            __phases = dict()
-            for index in sorted(indices.keys()):
-                orf = state["orfs"][indices[index]]
-                neworf = []
-
-                for segment in orf:
-                    if len(segment) == 3:
-                        assert segment[0] == "CDS"
-
-                        new_segment = (segment[0],
-                                       tuple(segment[1]),
-                                       int(segment[2]))
-                        self.combined_cds.append(new_segment[1])
-                        if index == 0:
-                            __phases[new_segment[1]] = new_segment[2]
-                    else:
-                        assert segment[0] != "CDS"
-                        new_segment = (segment[0],
-                                       tuple(segment[1]))
-                    neworf.append(new_segment)
-
-                self.internal_orfs.append(neworf)
-                self.phases = __phases
-            self.logger.debug("ORFs: %s", " ".join(str(_) for _ in self.internal_orfs))
-            self.selected_internal_orf_index = state["selected_orf"]
-        except (ValueError, IndexError):
-            raise CorruptIndex("Invalid values for ORFs of {}".format(self.id))
+        self.__check_and_load_dumped_orfs(state)
+        # state["external"] = dict((key, value) for key, value in self.external_scores.items())
+        for key, value in state["external"].items():
+            setattr(self.external_scores, key, value)
 
         calculated = state["calculated"]
-        del state["calculated"]
         self.__load_calculated_stats(calculated)
+        assert "_cdna_length" in self.__dict__
         for metric in self.get_modifiable_metrics():
             try:
                 setattr(self, metric, state[metric])
@@ -1314,12 +1289,47 @@ exon data is on a different chromosome, {exon_data.chrom}. \
             self.combined_utr = []
         self.finalize()
 
+    def __check_and_load_dumped_orfs(self, state):
+        """Private method to check that the dumped ORFs are as expected."""
+        try:
+            indices = dict((int(_), _) for _ in state["orfs"])
+            __phases = dict()
+            for index in sorted(indices.keys()):
+                orf = state["orfs"][indices[index]]
+                neworf = []
+
+                for segment in orf:
+                    if len(segment) == 3:
+                        assert segment[0] == "CDS"
+                        new_segment = (segment[0],
+                                       tuple(segment[1]),
+                                       int(segment[2]))
+                        self.combined_cds.append(new_segment[1])
+                        if index == 0:
+                            __phases[new_segment[1]] = new_segment[2]
+                    else:
+                        assert segment[0] != "CDS"
+                        new_segment = (segment[0],
+                                       tuple(segment[1]))
+                    neworf.append(new_segment)
+                self.internal_orfs.append(neworf)
+                self.phases = __phases
+            self.logger.debug("ORFs: %s", " ".join(str(_) for _ in self.internal_orfs))
+            self.selected_internal_orf_index = state["selected_orf"]
+        except (ValueError, IndexError):
+            raise CorruptIndex("Invalid values for ORFs of {}".format(self.id))
+        self.__selected_internal_orf_cds = []
+        for feature, (start, end), phase  in state["selected_internal_orf_cds"]:
+            self.__selected_internal_orf_cds.append((feature, (start, end), phase))
+        self.__selected_internal_orf_cds = tuple(self.__selected_internal_orf_cds)
+
     def __get_calculated_stats(self):
         """Private method to get the statistics calculated post-hoc.
         Necessary for the serialisation."""
 
         d = dict()
         self.finalize()
+        d["cdna_length"] = self.cdna_length
         d["exon_num"] = self.exon_num
         d["three_utr"] = self.three_utr
         d["five_utr"] = self.five_utr
@@ -1361,6 +1371,7 @@ exon data is on a different chromosome, {exon_data.chrom}. \
     def __load_calculated_stats(self, state):
         """Private method to reload the calculated stats from a dictionary dump."""
 
+        self._cdna_length = state["cdna_length"]
         self.__exon_num = state["exon_num"]
         self._three_utr = state["three_utr"]
         self.__five_utr = state["five_utr"]
@@ -1385,7 +1396,6 @@ exon data is on a different chromosome, {exon_data.chrom}. \
         self.__max_intron_length = state["max_intron_length"]
         self.__highest_cds_exon_number = state["highest_cds_exon_number"]
         self.__selected_cds_exons_fraction = state["selected_cds_exons_fraction"]
-        self.__cdna_length = state["cdna_length"]
         self.__cds_not_maximal = state["cds_not_maximal"]
         self.__cds_not_maximal_fraction = state["cds_not_maximal_fraction"]
         self._end_distance_from_tes = state["end_distance_from_tes"]
@@ -2345,14 +2355,14 @@ exon data is on a different chromosome, {exon_data.chrom}. \
     combined_utr_fraction.rtype = "float"
 
     def __calculate_cdna_length(self):
-        self.__cdna_length = int(sum([_[1] - _[0] + 1 for _ in self.exons]))
+        self._cdna_length = int(sum([_[1] - _[0] + 1 for _ in self.exons]))
 
     @Metric
     def cdna_length(self):
         """This property returns the length of the transcript."""
-        if self.finalized is False or self.__cdna_length is None:
+        if self.finalized is False or self._cdna_length is None:
             self.__calculate_cdna_length()
-        return self.__cdna_length
+        return self._cdna_length
 
     cdna_length.category = "cDNA"
     cdna_length.rtype = "int"

Mikado/_transcripts/transcript_methods/finalizing.py
@@ -690,7 +690,7 @@ def finalize(transcript):
                 else:
                     raise InvalidCDS(exc)
 
-    transcript.__cdna_length = None
+    transcript._cdna_length = None
     __basic_final_checks(transcript)
     # Sort the exons by start then stop
 

Mikado/tests/test_transcript_methods.py
@@ -10,7 +10,7 @@ from Mikado.configuration import MikadoConfiguration
 from sqlalchemy.engine import reflection
 import itertools
 
-from Mikado.utilities import Interval
+from Mikado.utilities import Interval, IntervalTree
 from ..configuration.configurator import load_and_validate_config
 from ..loci import Transcript
 from ..parsers.bed12 import BED12
@@ -997,6 +997,117 @@ Chr5	TAIR10	exon	5256	5576	.	-	.	Parent=AT5G01015.1"""
         new_t = Transcript()
         new_t.load_dict(t1_state)
         assert new_t.blast_hits == self.t1.blast_hits
+
+
+class TestPicklingAndToFromDict(unittest.TestCase):
+
+    def test_as_dict_and_load(self):
+        bed_line = "Chr5\t26584773\t26587782\tID=at_AT5G66610.2;coding=True;phase=0\t0\t+\t26585222\t26587755\t0\t10\t106,54,545,121,78,105,213,63,119,496\t0,446,571,1208,1443,1646,1864,2160,2310,2513"
+        original = Transcript(bed_line)
+        original.external.foo = 10
+
+        original.finalize()
+        recovered = Transcript()
+        dumped = original.as_dict()
+        recovered.load_dict(dumped, trust_orf=False)
+
+        missed_keys = []
+        new_keys = []
+        different_keys = []
+
+        for key in original.__dict__:
+            if not key in recovered.__dict__:
+                missed_keys.append(key)
+                continue
+            new, old = recovered.__dict__[key], original.__dict__[key]
+            if not isinstance(new, type(old)):
+                different_keys.append((key, str(new), str(old)))
+                continue
+            elif old != new:
+                if "external" in key:
+                    previous = dict((key, value) for key, value in old.items())
+                    now = dict((key, value) for key, value in new.items())
+                    different = False
+                    for key in set.union(set(previous.keys()), set(now.keys())):
+                        if key not in previous or key not in now or now[key] != previous[key]:
+                            different = True
+                            break
+                    if different:  # and max(len(now), len(previous)) > 0:
+                        different_keys.append((key, str(previous), str(now)))
+                elif isinstance(recovered.__dict__[key], IntervalTree):
+                    old_ivs = []
+                    new_ivs = []
+                    old.traverse(lambda iv: old_ivs.append(iv))
+                    new.traverse(lambda iv: new_ivs.append(iv))
+                    old_ivs = sorted(old_ivs)
+                    new_ivs = sorted(new_ivs)
+                    if old_ivs != new_ivs:
+                        different_keys.append((key, str(old_ivs), str(new_ivs)))
+                else:
+                    different_keys.append((key, str(old), str(new)))
+
+        for key in recovered.__dict__:
+            if not key in original.__dict__:
+                new_keys.append(key)
+
+        self.assertEqual(len(missed_keys), 0, "Missed keys: {}".format(", ".join([str(_) for _ in missed_keys])))
+        self.assertEqual(len(new_keys), 0, "New keys: {}".format(", ".join([str(_) for _ in new_keys])))
+        self.assertEqual(len(different_keys), 0, "New keys: {}".format(", ".join([str(_) for _ in different_keys])))
+
+    def test_pickling_comprehensive(self):
+        bed_line = "Chr5\t26584773\t26587782\tID=at_AT5G66610.2;coding=True;phase=0\t0\t+\t26585222\t26587755\t0\t10\t106,54,545,121,78,105,213,63,119,496\t0,446,571,1208,1443,1646,1864,2160,2310,2513"
+        original = Transcript(bed_line)
+        original.external.foo = 10
+
+        original.finalize()
+        recovered = pickle.loads(pickle.dumps(original))
+        missed_keys = []
+        new_keys = []
+        different_keys = []
+
+        for key in original.__dict__:
+            if not key in recovered.__dict__:
+                if key in ["engine", "session", "sessionmaker"]:
+                    # We expect these keys to be missing, they cannot be pickled and we are not trasmitting
+                    # the configuration.
+                    continue
+                missed_keys.append(key)
+                continue
+            new, old = recovered.__dict__[key], original.__dict__[key]
+            if not isinstance(new, type(old)):
+                different_keys.append((key, str(new), str(old)))
+                continue
+            elif old != new:
+                if "external" in key:
+                    previous = dict((key, value) for key, value in old.items())
+                    now = dict((key, value) for key, value in new.items())
+                    different = False
+                    for key in set.union(set(previous.keys()), set(now.keys())):
+                        if key not in previous or key not in now or now[key] != previous[key]:
+                            different = True
+                            break
+                    if different:  # and max(len(now), len(previous)) > 0:
+                        different_keys.append((key, str(previous), str(now)))
+                elif isinstance(recovered.__dict__[key], IntervalTree):
+                    old_ivs = []
+                    new_ivs = []
+                    old.traverse(lambda iv: old_ivs.append(iv))
+                    new.traverse(lambda iv: new_ivs.append(iv))
+                    old_ivs = sorted(old_ivs)
+                    new_ivs = sorted(new_ivs)
+                    if old_ivs != new_ivs:
+                        different_keys.append((key, str(old_ivs), str(new_ivs)))
+                else:
+                    different_keys.append((key, str(old), str(new)))
+
+        for key in recovered.__dict__:
+            if not key in original.__dict__:
+                new_keys.append(key)
+
+        self.assertEqual(len(missed_keys), 0, "Missed keys: {}".format(", ".join([str(_) for _ in missed_keys])))
+        self.assertEqual(len(new_keys), 0, "New keys: {}".format(", ".join([str(_) for _ in new_keys])))
+        self.assertEqual(len(different_keys), 0, "New keys: {}".format(", ".join([str(_) for _ in different_keys])))
+
 
 if __name__ == '__main__':
     unittest.main()

Mikado/transcripts/transcript.py
@@ -26,6 +26,7 @@ class Transcript(TranscriptBase):
 
     def __init__(self, *args, configuration=None, **kwargs):
         super().__init__(*args, **kwargs)
+        self.__configuration = None
         self.configuration = configuration
 
     @property
@@ -51,8 +52,8 @@ class Transcript(TranscriptBase):
 
         if configuration is None:
             configuration = default_config.copy()
-            assert isinstance(configuration, (MikadoConfiguration, DaijinConfiguration))
 
+        assert isinstance(configuration, (MikadoConfiguration, DaijinConfiguration))
         self.__configuration = configuration
 
     def __getstate__(self):
@@ -66,10 +67,10 @@ class Transcript(TranscriptBase):
         return state
 
     def __setstate__(self, state):
+        self.configuration = state.pop("configuration", None)
         self.__dict__.update(state)
         self._calculate_cds_tree()
         self._calculate_segment_tree()
-        # Set the logger to NullHandler
         self.logger = None
 
     def split_by_cds(self) -> List:

Mikado/utilities/namespace.py
@@ -36,6 +36,13 @@ class Namespace:
         self.__values[item] = self.__dict__[item]
         return self.__values[item]
 
+    def __setattr__(self, key, value):
+        if key.startswith("_"):
+            super().__setattr__(key, value)
+        else:
+            self.__dict__[key] = value
+            self.__values[key] = value
+
     def update(self, dictionary):
         self.__dict__.update(dictionary)
         self.__values.update(dictionary)
@@ -56,5 +63,4 @@ class Namespace:
 
     def items(self):
         return self.__values.items()
-
 

commit a8d6636713996707c2786f1c071650e7659ebe9e

Solved a bug introduced in d10bd07d5eb55f79927265e75c035aea9f132c45

Mikado/_transcripts/transcript_base.py
@@ -195,7 +195,7 @@ class TranscriptBase:
         self.__parent = []
         self.__combined_cds = []
         self.__exon_num = None
-
+        self.__configuration = None
         self.__combined_cds_length = 0
         self.__selected_cds = []
         self._cdna_length = None
@@ -218,6 +218,7 @@ class TranscriptBase:
 
         # Starting settings for everything else
         self.__chrom = None
+        self.__is_reference = False
         self.is_reference = is_reference
         self.feature = "transcript"
         self.__start, self.__end = None, None
@@ -880,6 +881,14 @@ exon data is on a different chromosome, {exon_data.chrom}. \
 
     is_reference.category = "External"
     is_reference.rtype = "bool"
+
+    @property
+    def configuration(self):
+        """
+        Configuration dictionary. It can be None.
+        :return:
+        """
+        return self.__configuration
 
     @property
     def frames(self):

Mikado/tests/locus_test.py
@@ -1130,6 +1130,7 @@ class MonoHolderTester(unittest.TestCase):
                           "CDS")
         self.t1.finalize()
         self.assertIs(self.t1.is_coding, True)
+        self.assertTrue(hasattr(self.t1, "configuration"))
 
     def testCdsOverlap(self):
 
@@ -1362,6 +1363,8 @@ class MonoHolderTester(unittest.TestCase):
                 t2.add_exons([(t2.start, 1560), (2801, t2.end)])
                 t2.add_exons([(1402 + phase, 1560), (2801, 3850 + phase)], "CDS")
                 self.assertIs(t2.is_coding, True)
+                self.assertIsInstance(self.t1, Transcript)
+                self.assertIsInstance(t2, Transcript)
                 self.assertIs(MonosublocusHolder.is_intersecting(self.t1,
                                                                  t2,
                                                                  cds_only=True,
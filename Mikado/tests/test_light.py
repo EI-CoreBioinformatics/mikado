94e6040745397f003444e32fa887f8a3deb40d97
Luca Venturini <luca.venturini@earlham.ac.uk>
2021-03-22 11:09:17 +0000

    Calling Mikado through subprocess rather than pkg_resources

--- Mikado/tests/test_light.py
+++ Mikado/tests/test_light.py
@@ -1,5 +1,6 @@
 """This test get executed before anything else in the GHA."""
 import gzip
+import subprocess
 import sys
 import unittest
 import os
@@ -32,40 +33,40 @@ class LightTest(unittest.TestCase):
         with tempfile.TemporaryDirectory(prefix="has_to_fail") as folder_one, \
                 tempfile.TemporaryDirectory(prefix="has_to_fail") as folder_two:
             for procs, folder in [(3, folder_one), (1, folder_two)]:
-                with self.subTest(procs=procs):
-                    json_file = os.path.join(folder, "mikado.yaml")
-                    db = os.path.join(folder, "mikado.db")
-                    log = "failed_serialise.log"
-                    uni_out = os.path.join(folder, "uniprot_sprot_plants.fasta")
-                    self.configuration.serialise.files.log = os.path.basename(log)
-                    self.configuration.multiprocessing_method = "fork"
-                    with gzip.open(uniprot, "rb") as uni, open(uni_out, "wb") as uni_out_handle:
-                        uni_out_handle.write(uni.read())
-                    with open(json_file, "wt") as json_handle:
-                        print_config(self.configuration, json_handle, output_format="yaml")
-                    with self.subTest(proc=procs):
-                        self.assertFalse(os.path.exists(db))
-                        sys.argv = [str(_) for _ in ["mikado", "serialise", "--json-conf", json_file,
-                                                     "--transcripts", transcripts, "--blast_targets", uni_out,
-                                                     "--log", log,
-                                                     "-od", folder,
-                                                     "--force",
-                                                     "--orfs", tmp_orf.name, "--junctions", junctions, "--xml", xml,
-                                                     "-p", procs, "-mo", mobjects, db,
-                                                     "--seed", "1078"]]
-                        log = os.path.join(folder, log)
-                        with self.assertRaises(SystemExit):
-                            pkg_resources.load_entry_point("Mikado", "console_scripts", "mikado")()
-                        self.assertTrue("failed" in log)
-                        self.assertTrue(os.path.exists(log), log)
-                        self.assertTrue(os.stat(log).st_size > 0, log)
-                        logged = [_.rstrip() for _ in open(log)]
-                        self.assertGreater(len(logged), 0)
-                        self.assertFalse(os.path.exists(db), logged)
-                        self.assertTrue(any(
-                            "Mikado serialise failed due to problems with the input data. Please check the logs." in line
-                            for line in logged), [print(line) for line in logged])
-                        self.assertTrue(any(
-                            "The provided ORFs do not match the transcripts provided and "
-                            "already present in the database." in line for line in logged),
-                            print("\n".join(logged)))
+                json_file = os.path.join(folder, "mikado.yaml")
+                db = os.path.join(folder, "mikado.db")
+                log = "failed_serialise.log"
+                uni_out = os.path.join(folder, "uniprot_sprot_plants.fasta")
+                self.configuration.serialise.files.log = os.path.basename(log)
+                self.configuration.multiprocessing_method = "fork"
+                with gzip.open(uniprot, "rb") as uni, open(uni_out, "wb") as uni_out_handle:
+                    uni_out_handle.write(uni.read())
+                with open(json_file, "wt") as json_handle:
+                    print_config(self.configuration, json_handle, output_format="yaml")
+
+                self.assertFalse(os.path.exists(db))
+                sys.argv = [str(_) for _ in ["mikado", "serialise", "--json-conf", json_file,
+                                             "--transcripts", transcripts, "--blast_targets", uni_out,
+                                             "--log", log,
+                                             "-od", folder,
+                                             "--force",
+                                             "--orfs", tmp_orf.name, "--junctions", junctions, "--xml", xml,
+                                             "-p", procs, "-mo", mobjects, db,
+                                             "--seed", "1078"]]
+                log = os.path.join(folder, log)
+                subprocess.call(sys.argv, shell=False)
+                # with self.assertRaises(SystemExit):
+                #     pkg_resources.load_entry_point("Mikado", "console_scripts", "mikado")()
+                self.assertTrue("failed" in log)
+                self.assertTrue(os.path.exists(log), log)
+                self.assertTrue(os.stat(log).st_size > 0, log)
+                logged = [_.rstrip() for _ in open(log)]
+                self.assertGreater(len(logged), 0)
+                self.assertFalse(os.path.exists(db), logged)
+                self.assertTrue(any(
+                    "Mikado serialise failed due to problems with the input data. Please check the logs." in line
+                    for line in logged), [print(line) for line in logged])
+                self.assertTrue(any(
+                    "The provided ORFs do not match the transcripts provided and "
+                    "already present in the database." in line for line in logged),
+                    print("\n".join(logged)))
